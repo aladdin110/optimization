cd2d27a28b764663f20457ad202728ba835e977b
alae <kouissi321@gmail.com>
2021-04-10 14:42:24 +0100

    nothing

--- library.h
+++ library.h
@@ -10,6 +10,8 @@ double data[4][11] = {{0,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1},//betai
                        {0,4,4,4,4,4,4,4,4,4,4},//ki
                        {0,0.1,0.2,0.3,0.15,0.1,0.25,0.05,0.27,0.12,0.18},//pi
                        {0,0.8,0.6,0.7,0.9,0.85,0.65,0.75,0.95,0.85,0.92}};//ri
+
+double
 int n = 10;
 int d = 1;
 int cP = 1;
@@ -21,5 +23,7 @@ void printdata(double data[4][10]);
 double Ni(double data[4][10], int i, int d);
 double Mi(double data[4][10], int i, double andes, int n);
 int landai(int landai);//landai appartient a {0,1}
+
+
 
 #endif //OPTIMIZATION_LIBRARY_H
--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include "library.h"
 
 int main() {
-    printdata(data);
-    double M = Mi(data,n,andes,n);
-    printf("M = %f\n",M);
+
     return 0;
 }
