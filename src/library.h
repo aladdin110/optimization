9e43d1d38c6933c26643817ffe99b406c10b1c78
alae <kouissi321@gmail.com>
2021-06-12 16:46:10 +0100

    LP done TODO : specify NLP of the model

--- src/library.h
+++ src/library.h
@@ -1,53 +1,25 @@
-//
-// Created by aladdin on 3/20/21.
-//
-
 #ifndef OPTIMIZATION_LIBRARY_H
 #define OPTIMIZATION_LIBRARY_H
+
+#define LINDO_HEADER "/home/aladdin/Main/Programms/LAPI-LINUX-64x86-13.0/lindoapi/include/lindo.h"
+#define LINDO_LICENSE "/home/aladdin/Main/Programms/LAPI-LINUX-64x86-13.0/lindoapi/license/lndapi130.lic"
+#define m 2
+
+
 #include <stdio.h>
 #include <stdlib.h>
-/*--------data--------*/
+#include <math.h>
+#include LINDO_HEADER
 
-/*
-//vecteur N
-double *N();
-
-//vecteur M
-double *M();
-
-//vecteur a
-double *a(double M[10], double N [10]);
-
-//vecteur b
-double *b(double M[10], double N [10]);
-
-//vecteur c
-double *c(double M[10], double N [10]);
-
-//vecteur e
-double *e(double M[10], double N [10]);
-
-//borne inferieur de ri~ definie dans (10)
+double N(int i);
+double M(int i);
+double a(double M[10], double N[10], int i);
+double b(double N[10], int i);
+double c(double M[10], double N[10], int i);
+double e(double M[10], int i);
 double borneInf_10(double a[10], double b[10], int i);
-
-//borne superieur de ri~ definie dans (10)
 double borneSup_10(double c[10], double e[10], int i);
 
-//vecteur p~
-double *ptilde(double rtilde[10],double a[10]);
-
-//vecteur q
-double *q(int lambda[10]);
-
-//vecteur d~
-double *dtilde(double q[10], int lambda[10]);
-
-//borne inferieur de ri~ definie dans (7)
-double borneInf_7(double a[10], double rtilde[10], double b[10], int i);
-
-//borne superieur de ri~ definie dans (7)
-double borneSup_7(double a[10], double rtilde[10], double b[10], int i);
-*/
 
 double preciser(double x);
 
